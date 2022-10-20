eb6b591d4bb7024b29667a06eaf4130204218137
Miranda Johnson <miranda@after-eng.com>
2022-10-20 09:57:46 -0600

    production set up

--- jobs/admin.py
+++ jobs/admin.py
@@ -4,7 +4,7 @@ from jobs.models import Job, Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('qb_number', 'eng_number', 'part_name')
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
